70d34e8a4b49754b54717af79bc18508c1d914da
Mark Rivers <rivers@cars.uchicago.edu>
2020-04-22 19:12:50 -0500

    Many additions

--- tomoscan/tomoscan_13bm.py
+++ tomoscan/tomoscan_13bm.py
@@ -1,25 +1,20 @@
 from epics import PV
 from tomoscan import tomoscan
+import time
+import math
 
 class tomoscan_13bm(tomoscan):
 
     def __init__(self, autoSettingsFile, macros=[]):
         tomoscan.__init__(self, autoSettingsFile, macros)
         
-
-    def configureCamera(self):
-        a=0
-    
-    def collectDarkFields(self):
-        a=0
-
-    def collectFlatFields(self):
-        a=0
-      
-    def collectNormalFields(self):
-        a=0
-
-
+        # Set the detector running in FreeRun mode
+        self.setTriggerMode('FreeRun', 1)
+        # Enable auto-increment on file writer
+        self.epicsPVs['FPAutoIncrement'].put('Yes')
+        # Set the SIS output pulse width to 100 us
+        self.epicsPVs['MCSLNEOutputWidth'].put(0.0001)
+        
     def setTriggerMode(self, triggerMode, numImages):
         if (triggerMode == 'FreeRun'):
             self.epicsPVs['CamImageMode'].put('Continuous', wait=True)
@@ -34,7 +29,7 @@ class tomoscan_13bm(tomoscan):
             # Set number of MCS channels, NumImages, and NumCapture
             self.epicsPVs['MCSStopAll'].put(1, wait=True)
             self.epicsPVs['MCSNuseAll'].put(numImages, wait=True)
-            self.epicsPVs['HdfNumCapture'].put(numImages, wait=True)
+            self.epicsPVs['FPNumCapture'].put(numImages, wait=True)
   
         if (triggerMode == 'MCSExternal'):
             # Put MCS in external trigger mode
@@ -44,3 +39,78 @@ class tomoscan_13bm(tomoscan):
             self.epicsPVs['MCSChannelAdvance'].put('Internal', wait=True)
             time = self.computeFrameTime()
             self.epicsPVs['MCSDwell'].put(time, wait=True)
+
+    def collectNFrames(self, numFrames, save=True):
+        # This is called when collecting dark fields or flat fields
+        self.setTriggerMode('MCSInternal', numFrames)
+        if (save):
+            self.epicsPVs['FPCapture'].put('Capture')
+        self.epicsPVs['CamAcquire'].put('Acquire')
+        # Wait for detector and file plugin to be ready
+        time.sleep(0.5)
+        # Start the MCS
+        self.epicsPVs['MCSEraseStart'].put(1)
+        collectionTime = self.epicsPVs['MCSDwell'].value * numFrames
+        self.waitCameraDone(collectionTime + 5.0)
+       
+    def beginScan(self):
+        # Stop the camera since it could be in free-run mode
+        self.epicsPVs['CamAcquire'].put(0, wait=True)
+        # Set the exposure time
+        exposureTime = self.epicsPVs['ExposureTime'].value
+        self.epicsPVs['CamAcquireTime'].put(exposureTime)
+        # Set the file path, file name and file number
+        self.epicsPVs['FPFilePath'].put(self.epicsPVs['FilePath'].value)
+        self.epicsPVs['FPFileName'].put(self.epicsPVs['FileName'].value)
+        self.epicsPVs['FPFileNumber'].put(1)
+        # Need to collect 3 dummy frames after changing camera to triggered mode
+        self.collectNFrames(3, False)
+        # The MCS LNE output stays low after stopping MCS for up to the exposure time = LNE output width
+        # Need to wait for the exposure time
+        time.sleep(exposureTime)
+
+    def endScan(self):
+        # Save the configuration
+        filePath = self.epicsPVs['FilePath'].value
+        fileName = self.epicsPVs['FileName'].value
+        self.saveConfiguration(filePath + fileName + '.config')
+        # Put the camera back in FreeRun mode
+        self.setTriggerMode('FreeRun', 1)
+
+    def collectDarkFields(self):
+        self.collectNFrames(self.epicsPVs['NumDarkFields'].value)
+
+    def collectFlatFields(self):
+        self.collectNFrames(self.epicsPVs['NumFlatFields'].value)
+      
+    def collectNormalFields(self):
+        rotationStart = self.epicsPVs['RotationStart'].value
+        rotationStep = self.epicsPVs['RotationStep'].value
+        numAngles = self.epicsPVs['NumAngles'].value
+        rotationStop = rotationStart + (rotationStep * numAngles)
+        maxSpeed = self.epicsPVs['RotationMaxSpeed'].value
+        self.epicsPVs['RotationSpeed'].put(maxSpeed)
+        # Start angle is decremented a half rotation step so scan is centered on rotationStart
+        # The SIS does not put out pulses until after one dwell period so need to back up an additional angle step
+        self.epicsPVs['Rotation'].put((rotationStart - 1.5 * rotationStep), wait=True)
+        # Compute and set the motor speed
+        timePerAngle = self.computeFrameTime()
+        speed = rotationStep / timePerAngle
+        stepsPerDeg = abs(math.round(1./self.epicsPVs['RotationResolution'].value))
+        motorSpeed = math.floor((speed * stepsPerDeg)) / stepsPerDeg
+        self.epicsPVs['RotationSpeed'].put(motorSpeed)
+        # Set the external prescale according to the step size, use motor resolution steps per degree (user unit)
+        self.epicsPVs['MCSStopAll'].put(1, wait=True)
+        prescale = math.floor(abs(rotationStep  * stepsPerDeg))
+        self.epicsPVs['MCSPrescale'].put(prescale, wait=True)
+        self.setTriggerMode('MCSExternal', numAngles)
+        # Start capturing in file plugin
+        self.epicsPVs['FPCapture'].put('Capture')
+        # Start the camera
+        self.epicsPVs['CamAcquire'].put('Acquire')
+        # Start the MCS
+        self.epicsPVs['MCSEraseStart'].put(1)
+        # Wait for detector, file plugin, and MCS to be ready
+        time.sleep(0.5)
+        # Start the rotation motor
+        self.epicsPVs['Rotation'].put(rotationStop)
