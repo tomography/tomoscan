e9bfb9ac000b31866036badd93e3eb64fb42c5e8
Mark Rivers <rivers@cars.uchicago.edu>
2020-04-26 20:46:14 -0500

    Added docutils documentaion

--- tomoscan/tomoscan_13bm.py
+++ tomoscan/tomoscan_13bm.py
@@ -4,6 +4,16 @@ import time
 import math
 
 class tomoscan_13bm(tomoscan):
+    """Derived class used for tomography scanning with EPICS at APS beamline 13-BM-D
+
+    Parameters
+    ----------
+    pvFiles : list of str
+        List of files containing EPICS pvNames to be used.
+    macros : dict
+        Dictionary of macro definitions to be substituted when
+        reading the pvFiles
+    """
 
     def __init__(self, pvFile, macros=[]):
         super().__init__(pvFile, macros)
@@ -16,6 +26,18 @@ class tomoscan_13bm(tomoscan):
         self.epicsPVs['MCSLNEOutputWidth'].put(0.0001)
         
     def setTriggerMode(self, triggerMode, numImages):
+        """Sets the trigger mode SIS3820 and the camera. 
+
+        Parameters
+        ----------
+        triggerMode : str
+            Choices are: "FreeRun", "MCSInternal", or "MCSExternal"
+
+        numImages : int
+            Number of images to collect.  Ignored if triggerMode="FreeRun".
+            This is used to set the ``NuseAll`` PV of the SIS MCS, the ``NumImages`` PV of the camera,
+            and the ``NumCapture`` PV of the file plugin. 
+        """
         if (triggerMode == 'FreeRun'):
             self.epicsPVs['CamImageMode'].put('Continuous', wait=True)
             self.epicsPVs['CamTriggerMode'].put('Off', wait=True)
@@ -42,6 +64,16 @@ class tomoscan_13bm(tomoscan):
             self.epicsPVs['MCSDwell'].put(time, wait=True)
 
     def collectNFrames(self, numFrames, save=True):
+        """Collects numFrames images in "MCSInternal" trigger mode for dark fields and flat fields.
+
+        Parameters
+        ----------
+        numFrames : int
+            Number of frames to collect.
+            
+        save : bool, optional
+            False to disable saving frames with the file plugin.
+        """
         # This is called when collecting dark fields or flat fields
         self.setTriggerMode('MCSInternal', numFrames)
         if (save):
@@ -55,6 +87,19 @@ class tomoscan_13bm(tomoscan):
         self.waitCameraDone(collectionTime + 5.0)
        
     def beginScan(self):
+        """Performs the operations needed at the very start of a scan.
+       
+        This does the following:
+       
+        - Calls the base class method.
+       
+        - Collects 3 dummy images with ``collectNFrames``.  This is required when switching from
+          "FreeRun" to triggered mode on the Point Grey camera.
+
+        - Waits for 1 exposure time because the MCS LNE output stays low for up to the exposure time.
+
+        """
+
         # Call the base class method
         super().beginScan()
         # Need to collect 3 dummy frames after changing camera to triggered mode
@@ -64,6 +109,21 @@ class tomoscan_13bm(tomoscan):
         time.sleep(self.epicsPVs['ExposureTime'].value)
 
     def endScan(self):
+        """Performs the operations needed at the very end of a scan.
+        
+        This does the following:
+       
+        - Calls ``saveConfiguration()``.
+       
+        - Put the camera back in "FreeRun" mode and acquiring so the user sees live images.
+       
+        - Sets the speed of the rotation stage back to the maximum value.
+       
+        - Calls ``moveSampleIn()``.
+       
+        - Calls the base class method.
+        """
+
         # Save the configuration
         filePath = self.epicsPVs['FilePath'].get(as_string=True)
         fileName = self.epicsPVs['FileName'].get(as_string=True)
@@ -79,14 +139,52 @@ class tomoscan_13bm(tomoscan):
         super().endScan()
 
     def collectDarkFields(self):
+        """Collects dark field images.
+       
+        Calls ``collectNFrames()`` with the number of images specified by the ``NumDarkFields`` PV. 
+        """
+
         self.epicsPVs['ScanStatus'].put('Collecting dark fields')
         self.collectNFrames(self.epicsPVs['NumDarkFields'].value)
 
     def collectFlatFields(self):
+        """Collects flat field images.
+       
+        Calls ``collectNFrames()`` with the number of images specified by the ``NumFlatFields`` PV. 
+        """
+
         self.epicsPVs['ScanStatus'].put('Collecting flat fields')
         self.collectNFrames(self.epicsPVs['NumFlatFields'].value)
       
     def collectProjections(self):
+        """Collects projections in fly scan mode.
+       
+        This does the following:
+        - Sets the ``ScanStatus`` PV.
+        
+        - Moves the rotation motor to the position specified by the ``RotationStart`` PV
+          minus a delta angle so that the first projection is centered on that position, 
+          and also compensates for the behavior of the SIS MCS.
+        
+        - Computes and sets the speed of the rotation motor so that it reaches the next projection
+          angle just after the current exposure and readout are complete.
+        
+        - Sets the prescale factor of the MCS to be the number of motor pulses per rotation angle.
+          The MCS is set to external trigger mode and is triggered by the stepper motor pulses for the
+          rotation stage.
+        
+        - Starts the file plugin capturing in stream mode.
+        
+        - Starts the camera acquiring in external trigger mode.
+        
+        - Starts the MCS acquiring.
+        
+        - Moves the rotation motor to the position specified by the ``RotationStop`` PV.
+          This triggers the acquisition of the camera.
+        
+        - Calls ``waitCameraDone()``.
+        """
+
         self.epicsPVs['ScanStatus'].put('Collecting projections')
         rotationStart = self.epicsPVs['RotationStart'].value
         rotationStep = self.epicsPVs['RotationStep'].value
