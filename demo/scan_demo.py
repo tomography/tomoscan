d279cc44d6995acda13ae7d11114afa9d81cc9aa
Mark Rivers <rivers@cars.uchicago.edu>
2020-05-03 16:57:37 -0500

    Set the FileNumber and AutoIncrement values

--- demo/scan_demo.py
+++ demo/scan_demo.py
@@ -19,9 +19,12 @@ def scan_demo(tomo_prefix, exposure_time, scan_pv, start, step, points):
     """
 
     epics.caput(tomo_prefix + 'ExposureTime', exposure_time, wait=True)
+    file_plugin_prefix = epics.caget(tomo_prefix + 'FilePluginPVPrefix')
+    # Set the initial file number back to 1 and make sure AutoIncrement is enables
+    epics.caput(file_plugin_prefix + 'FileNumber', 1)
+    epics.caput(file_plugin_prefix + 'AutoIncrement', 'Yes')
 
     for i in range(1, points+1):
         epics.caput(scan_pv, start + step*i, wait=True)
-        epics.caput(tomo_prefix + 'FileName', 'Test_'+str(i), wait=True)
         epics.caput(tomo_prefix + 'StartScan', 1, wait=True, timeout=100)
-        print('Completed scan %d' % i)
+        print('Completed dataset %s' % epics.caget(file_plugin_prefix + 'FullFileName_RBV', as_string=True))
