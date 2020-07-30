bda9b00e758dc7410c822d3314f9dfc35c9fc6d2
Mark Rivers <rivers@cars.uchicago.edu>
2020-07-30 15:16:54 -0500

    Fix frame_time when exposure time is 2 seconds; needs 2.002 between triggers

--- tomoscan/tomoscan.py
+++ tomoscan/tomoscan.py
@@ -775,7 +775,9 @@ class TomoScan():
         # Add 1 or 5 ms to exposure time for margin
         if exposure > 2.3:
             frame_time = exposure + .005
-        else:
+        else if exposure > 1.0:
+            frame_time = exposure + .002
+        else
             frame_time = exposure + .001
 
         # If the time is less than the readout time then use the readout time
