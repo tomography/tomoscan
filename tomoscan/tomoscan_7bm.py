9a4efa73741955e27a0a81e587a7446f2c75d4c5
aps-7bm <aps.7bm@gmail.com>
2020-09-16 14:05:04 -0500

    Recent template and Python file changes during initial testing.

--- tomoscan/tomoscan_7bm.py
+++ tomoscan/tomoscan_7bm.py
@@ -7,6 +7,7 @@
 """
 import time
 import os
+import math
 import h5py 
 from pathlib import Path
 import numpy as np
@@ -132,7 +133,7 @@ class TomoScan7BM(TomoScan):
         else: # set camera to external triggering
             # These are just in case the scan aborted with the camera in another state
             self.epics_pvs['CamTriggerMode'].put('Off', wait=True)
-            self.epics_pvs['CamTriggerSource'].put('Line0', wait=True)
+            self.epics_pvs['CamTriggerSource'].put('Line2', wait=True)
             self.epics_pvs['CamTriggerOverlap'].put('ReadOut', wait=True)
             self.epics_pvs['CamExposureMode'].put('Timed', wait=True)
 
@@ -186,8 +187,8 @@ class TomoScan7BM(TomoScan):
         time.sleep(0.1)
 
         # Program the stage driver to provide PSO pulses
-        compute_positions()
-        program_PSO()
+        self.compute_positions()
+        self.program_PSO()
 
         # Compute total number of frames to capture
         self.total_images = self.num_angles
@@ -236,7 +237,7 @@ class TomoScan7BM(TomoScan):
         self.set_trigger_mode('FreeRun', 1)
         # Set the rotation speed to maximum
         self.epics_pvs['RotationSpeed'].put(self.max_rotation_speed)
-        cleanup_PSO()
+        self.cleanup_PSO()
         # Move the sample in.  Could be out if scan was aborted while taking flat fields
         self.move_sample_in()
         # Call the base class method
@@ -249,6 +250,10 @@ class TomoScan7BM(TomoScan):
         log.info('add theta')
         self.theta = np.linspace(self.rotation_start, self.rotation_stop, self.num_angles)
         full_file_name = self.epics_pvs['FPFullFileName'].get(as_string=True)
+        print(full_file_name)
+        file_name_path = Path(full_file_name)
+        for i in file_name_path.parent.iterdir():
+            print(i)
         if os.path.exists(full_file_name):
             try:
                 f = h5py.File(full_file_name, "a")
@@ -361,10 +366,10 @@ class TomoScan7BM(TomoScan):
     def program_PSO(self):
         '''Performs programming of PSO output on the Aerotech driver.
         '''
-        overall_sense, user_direction = _compute_senses()
+        overall_sense, user_direction = self._compute_senses()
         asynRec = self.epics_pvs['PSOAsyn']
-        pso_axis = epics_pvs['PSOAxisName'].get(as_string=True)
-        pso_input = epics_pvs['PSOEncoderInput'].get(as_string=True)
+        pso_axis = self.epics_pvs['PSOAxisName'].get(as_string=True)
+        pso_input = int(self.epics_pvs['PSOEncoderInput'].get(as_string=True))
 
         #Place the motor at the position where the first PSO pulse should be triggered
         self.epics_pvs['RotationSpeed'].put(self.max_rotation_speed)
@@ -378,20 +383,20 @@ class TomoScan7BM(TomoScan):
         asynRec.put('PSOOUTPUT %s CONTROL 1' % pso_axis, wait=True, timeout=10.0)
         time.sleep(0.05)
         #Set a pulse 10 us long, 20 us total duration, so 10 us on, 10 us off
-        self.asynRec.put('PSOPULSE %s TIME 20,10' % pso_axis, wait=True, timeout=10.0)
+        asynRec.put('PSOPULSE %s TIME 20,10' % pso_axis, wait=True, timeout=10.0)
         time.sleep(0.05)
         #Set the pulses to only occur in a specific window
-        self.asynRec.put('PSOOUTPUT %s PULSE WINDOW MASK' % pso_axis, wait=True, timeout=10.0)
+        asynRec.put('PSOOUTPUT %s PULSE WINDOW MASK' % pso_axis, wait=True, timeout=10.0)
         time.sleep(0.05)
         #Set which encoder we will use.  3 = the MXH (encoder multiplier) input, which is what we generally want
-        self.asynRec.put('PSOTRACK %s INPUT %d' % (pso_axis, pso_input), wait=True, timeout=10.0)
+        asynRec.put('PSOTRACK %s INPUT %d' % (pso_axis, pso_input), wait=True, timeout=10.0)
         time.sleep(0.05)
         #Set the distance between pulses.  Do this in encoder counts.
-        self.asynRec.put('PSODISTANCE %s FIXED %d' % (pso_axis,
+        asynRec.put('PSODISTANCE %s FIXED %d' % (pso_axis,
                              self.epics_pvs['EncoderPulsesPerStep'].get()), wait=True, timeout=10.0)
         time.sleep(0.05)
         #Which encoder is being used to calculate whether we are in the window.  1 for single axis
-        self.asynRec.put('PSOWINDOW %s 1 INPUT %d' % (pso_axis, pso_input), wait=True, timeout=10.0)
+        asynRec.put('PSOWINDOW %s 1 INPUT %d' % (pso_axis, pso_input), wait=True, timeout=10.0)
         time.sleep(0.05)
 
         #Calculate window function parameters.  Must be in encoder counts, and is 
@@ -406,10 +411,10 @@ class TomoScan7BM(TomoScan):
         else:
             window_end = range_start
             window_start = window_end - range_length
-        self.asynRec.put('PSOWINDOW %s 1 RANGE %d,%d' % (pso_axis, window_start-5, window_end+5), wait=True, timeout=10.0)
+        asynRec.put('PSOWINDOW %s 1 RANGE %d,%d' % (pso_axis, window_start-5, window_end+5), wait=True, timeout=10.0)
         #Arm the PSO
         time.sleep(0.05)
-        self.asynRec.put('PSOCONTROL %s ARM' % pso_axis, wait=True, timeout=10.0)
+        asynRec.put('PSOCONTROL %s ARM' % pso_axis, wait=True, timeout=10.0)
 
 
     def cleanup_PSO(self):
@@ -418,10 +423,10 @@ class TomoScan7BM(TomoScan):
         '''
         log.info('Cleaning up PSO programming and setting to retrace speed.')
         asynRec = self.epics_pvs['PSOAsyn']
-        pso_axis = epics_pvs['PSOAxisName'].get(as_string=True)
-        pso_input = epics_pvs['PSOEncoderInput'].get(as_string=True)
-        self.asynRec.put('PSOWINDOW %s OFF' % pso_axis, wait=True)
-        self.asynRec.put('PSOCONTROL %s OFF' % pso_axis, wait=True)
+        pso_axis = self.epics_pvs['PSOAxisName'].get(as_string=True)
+        pso_input = self.epics_pvs['PSOEncoderInput'].get(as_string=True)
+        asynRec.put('PSOWINDOW %s OFF' % pso_axis, wait=True)
+        asynRec.put('PSOCONTROL %s OFF' % pso_axis, wait=True)
 
 
     def _compute_senses(self):
@@ -434,9 +439,7 @@ class TomoScan7BM(TomoScan):
         #Get motor direction (dial vs. user); convert (0,1) = (pos, neg) to (1, -1)
         motor_dir = 1 - int(self.epics_pvs['RotationDirection'].get()) * 2
         #Figure out whether motion is in positive or negative direction in user coordinates
-        req_start = float(epics_pvs['startPos'].get())
-        req_end = float(epics_pvs['endPos'].get())
-        user_direction = 1 if req_end > req_start else -1
+        user_direction = 1 if self.rotation_stop > self.rotation_start else -1
         #Figure out overall sense: +1 if motion in + encoder direction, -1 otherwise
         return user_direction * motor_dir * encoder_dir, user_direction
 
@@ -449,7 +452,7 @@ class TomoScan7BM(TomoScan):
         Computes the taxi distance at the beginning and end of scan to allow
         the stage to accelerate to speed.
         '''
-        overall_sense, user_direction = _compute_senses()
+        overall_sense, user_direction = self._compute_senses()
         #Get the distance needed for acceleration = 1/2 a t^2 = 1/2 * v * t
         motor_accl_time = float(self.epics_pvs['RotationAccelTime'].get())    #Acceleration time in s
         accel_dist = motor_accl_time / 2.0 * float(self.motor_speed) 
