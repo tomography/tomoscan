ac7fdb858b3b8f7328bc5671d7e9c080a999213c
Mark Rivers <rivers@cars.uchicago.edu>
2021-01-17 17:58:35 -0600

    New file

---
+++ iocBoot/iocTomoScan_13BM_PSO/start_tomoscan.py
@@ -0,0 +1,9 @@
+# This script creates an object of type TomoScan13BM for doing tomography scans at APS beamline 13-BM-D
+# To run this script type the following:
+#     python -i start_tomoscan_13bm.py
+# The -i is needed to keep Python running, otherwise it will create the object and exit
+from tomoscan.tomoscan_13bm_pso import TomoScan13BMPSO
+ts = TomoScan13BMPSO(["../../db/tomoScan_settings.req",
+                      "../../db/tomoScan_settings.req", 
+                      "../../db/tomoScan_13BM_PSO_settings.req"], 
+                     {"$(P)":"TSTest:", "$(R)":"TS1:"})
