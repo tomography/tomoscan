76d7b1b39c2b9f37e8af045bac9d19f33c77e9d7
Mark Rivers <rivers@cars.uchicago.edu>
2021-01-23 11:22:07 -0600

    Renamed tomoscan13_bm.py to tomoscan_13bm_mcs.py

--- tomoscan/__init__.py
+++ tomoscan/__init__.py
@@ -3,7 +3,7 @@ __version__ = '0.1'
 from tomoscan.tomoscan import *
 from tomoscan.tomoscan_pso import *
 from tomoscan.tomoscan_13bm_pso import *
-from tomoscan.tomoscan_13bm import *
+from tomoscan.tomoscan_13bm_mcs import *
 from tomoscan.tomoscan_2bm import *
 from tomoscan.tomoscan_stream_2bm import *
 from tomoscan.tomoscan_7bm import *
