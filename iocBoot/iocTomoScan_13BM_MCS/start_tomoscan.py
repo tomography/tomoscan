9c56403d03e163c21fe1a556743706ac711c61d4
Mark Rivers <rivers@cars.uchicago.edu>
2021-01-27 07:45:49 -0600

    Use new TomoScan13BM_MCS class

--- iocBoot/iocTomoScan_13BM_MCS/start_tomoscan.py
+++ iocBoot/iocTomoScan_13BM_MCS/start_tomoscan.py
@@ -1,6 +1,6 @@
-# This script creates an object of type TomoScan13BM for doing tomography scans at APS beamline 13-BM-D
+# This script creates an object of type TomoScan13BM_MCS for doing tomography scans at APS beamline 13-BM-D
 # To run this script type the following:
-#     python -i start_tomoscan_13bm.py
+#     python -i start_tomoscan.py
 # The -i is needed to keep Python running, otherwise it will create the object and exit
-from tomoscan.tomoscan_13bm import TomoScan13BM
-ts = TomoScan13BM(["../../db/tomoScan_settings.req","../../db/tomoScan_13BM_settings.req"], {"$(P)":"TSTest:", "$(R)":"TS1:"})
+from tomoscan.tomoscan_13bm_mcs import TomoScan13BM_MCS
+ts = TomoScan13BM_MCS(["../../db/tomoScan_settings.req","../../db/tomoScan_13BM_settings.req"], {"$(P)":"TSTest:", "$(R)":"TS1:"})
