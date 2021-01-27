2d1b39d8cbc356250f2d1b83a356e57e8b45eaa2
Mark Rivers <rivers@cars.uchicago.edu>
2021-01-27 07:41:23 -0600

    Update which settings.req files are used

--- iocBoot/iocTomoScan_13BM_PSO/start_tomoscan.py
+++ iocBoot/iocTomoScan_13BM_PSO/start_tomoscan.py
@@ -1,9 +1,9 @@
-# This script creates an object of type TomoScan13BM for doing tomography scans at APS beamline 13-BM-D
+# This script creates an object of type TomoScan13BM_PSO for doing tomography scans at APS beamline 13-BM-D
 # To run this script type the following:
-#     python -i start_tomoscan_13bm.py
+#     python -i start_tomoscan.py
 # The -i is needed to keep Python running, otherwise it will create the object and exit
 from tomoscan.tomoscan_13bm_pso import TomoScan13BM_PSO
 ts = TomoScan13BM_PSO(["../../db/tomoScan_settings.req",
-                      "../../db/tomoScan_settings.req", 
-                      "../../db/tomoScan_13BM_PSO_settings.req"], 
-                     {"$(P)":"TSTest:", "$(R)":"TS1:"})
+                       "../../db/tomoScan_PSO_settings.req", 
+                       "../../db/tomoScan_13BM_settings.req"], 
+                      {"$(P)":"TSTest:", "$(R)":"TS1:"})
