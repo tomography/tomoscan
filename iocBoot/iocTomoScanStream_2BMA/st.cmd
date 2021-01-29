a0ff886f9aa3602d7a8366cd78a8e717add826b8
Francesco De Carlo <decarlof@gmail.com>
2021-01-28 18:36:05 -0600

    added iocTomoScanStream

---
+++ iocBoot/iocTomoScanStream_2BMA/st.cmd
@@ -0,0 +1,25 @@
+< envPaths
+
+epicsEnvSet("P", "2bma:")
+epicsEnvSet("R", "TomoScanStream:")
+
+## Register all support components
+
+# Use these lines to run the locally built tomoScanApp
+dbLoadDatabase "../../dbd/tomoScanApp.dbd"
+tomoScanApp_registerRecordDeviceDriver pdbbase
+
+# Use these lines to run the xxx application on APSshare.
+#dbLoadDatabase "/APSshare/epics/synApps_6_1/support/xxx-R6-1/dbd/iocxxxLinux.dbd"
+#iocxxxLinux_registerRecordDeviceDriver pdbbase
+
+
+dbLoadTemplate("tomoScanStream.substitutions")
+
+< save_restore.cmd
+save_restoreSet_status_prefix($(P))
+dbLoadRecords("$(AUTOSAVE)/asApp/Db/save_restoreStatus.db", "P=$(P)")
+
+iocInit
+
+create_monitor_set("auto_settings.req", 30, "P=$(P),R=$(R)")
--- iocBoot/iocTomoScanStream_2BMA/start_tomoscan.py
+++ iocBoot/iocTomoScanStream_2BMA/start_tomoscan.py
@@ -3,4 +3,4 @@
 #     python -i start_tomoscan_stream_2bm.py
 # The -i is needed to keep Python running, otherwise it will create the object and exit
 from tomoscan.tomoscan_stream_2bm import TomoScanStream2BM
-ts = TomoScanStream2BM(["../../db/tomoScan_settings.req","../../db/tomoScan_2BM_settings.req"], {"$(P)":"2bma:", "$(R)":"TomoScan:"})
+ts = TomoScanStream2BM(["../../db/tomoScan_settings.req","../../db/tomoScan_2BM_settings.req"], {"$(P)":"2bma:", "$(R)":"TomoScanStream:"})
