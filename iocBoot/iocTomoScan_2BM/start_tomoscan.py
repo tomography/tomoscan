b685112217ee07550c198ac466da49bab726c7bd
Mark Rivers <rivers@cars.uchicago.edu>
2021-01-30 09:08:59 -0600

    Changes to use the new TomoScanPSO base class

--- iocBoot/iocTomoScan_2BM/start_tomoscan.py
+++ iocBoot/iocTomoScan_2BM/start_tomoscan.py
@@ -3,4 +3,6 @@
 #     python -i start_tomoscan_2bm.py
 # The -i is needed to keep Python running, otherwise it will create the object and exit
 from tomoscan.tomoscan_2bm import TomoScan2BM
-ts = TomoScan2BM(["../../db/tomoScan_settings.req","../../db/tomoScan_2BM_settings.req"], {"$(P)":"2bma:", "$(R)":"TomoScan:"})
+ts = TomoScan2BM(["../../db/tomoScan_settings.req",
+                  "../../db/tomoScan_2BM_settings.req"], 
+                 {"$(P)":"2bma:", "$(R)":"TomoScan:"})
