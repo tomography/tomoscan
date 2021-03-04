46b93132aa81a2fd4bcd007c3b1558242248309d
Mark Rivers <rivers@cars.uchicago.edu>
2021-03-04 14:03:56 -0600

    Update for MCS

--- iocBoot/iocTomoScan_13BM_MCS/start_tomoscan.py
+++ iocBoot/iocTomoScan_13BM_MCS/start_tomoscan.py
@@ -1,6 +1,9 @@
-# This script creates an object of type TomoScan13BM_MCS for doing tomography scans at APS beamline 13-BM-D
+# This script creates an object of type TomoScan13BM_PSO for doing tomography scans at APS beamline 13-BM-D
 # To run this script type the following:
 #     python -i start_tomoscan.py
 # The -i is needed to keep Python running, otherwise it will create the object and exit
 from tomoscan.tomoscan_13bm_mcs import TomoScan13BM_MCS
-ts = TomoScan13BM_MCS(["../../db/tomoScan_settings.req","../../db/tomoScan_13BM_settings.req"], {"$(P)":"TSTest:", "$(R)":"TS1:"})
+ts = TomoScan13BM_MCS(["../../db/tomoScan_settings.req",
+                       "../../db/tomoScan_13BM_MCS_settings.req", 
+                       "../../db/tomoScan_13BM_settings.req"], 
+                      {"$(P)":"13BMDPG2:", "$(R)":"TS:"})
