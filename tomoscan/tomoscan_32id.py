d20d3e2bd3e25b255e2935eb99e7774abcb516d5
Francesco De Carlo <decarlof@gmail.com>
2021-06-01 17:07:44 -0500

    work in progress

--- tomoscan/tomoscan_32id.py
+++ tomoscan/tomoscan_32id.py
@@ -48,6 +48,253 @@ class TomoScan32ID(TomoScanPSO):
 
         log.setup_custom_logger("./tomoscan.log")
 
+    def set_trigger_mode(self, trigger_mode, num_images):
+        """Sets the trigger mode SIS3820 and the camera.
+
+        Parameters
+        ----------
+        trigger_mode : str
+            Choices are: "FreeRun", "Internal", or "PSOExternal"
+
+        num_images : int
+            Number of images to collect.  Ignored if trigger_mode="FreeRun".
+            This is used to set the ``NumImages`` PV of the camera.
+        """
+        camera_model = self.epics_pvs['CamModel'].get(as_string=True)
+        if(camera_model=='Grasshopper3 GS3-U3-51S5M'):        
+            self.set_trigger_mode_grasshopper(trigger_mode, num_images)
+        else:
+            log.error('Camera is not supported')
+            exit(1)
+
+    def set_trigger_mode_grasshopper(self, trigger_mode, num_images):
+        self.epics_pvs['CamAcquire'].put('Done') ###
+        self.wait_pv(self.epics_pvs['CamAcquire'], 0) ###
+        log.info('set trigger mode: %s', trigger_mode)
+        if trigger_mode == 'FreeRun':
+            self.epics_pvs['CamImageMode'].put('Continuous', wait=True)
+            self.epics_pvs['CamTriggerMode'].put('Off', wait=True)
+            self.wait_pv(self.epics_pvs['CamTriggerMode'], 0)
+            # self.epics_pvs['CamAcquire'].put('Acquire')
+        elif trigger_mode == 'Internal':
+            self.epics_pvs['CamTriggerMode'].put('Off', wait=True)
+            self.wait_pv(self.epics_pvs['CamTriggerMode'], 0)
+            self.epics_pvs['CamImageMode'].put('Multiple')            
+            self.epics_pvs['CamNumImages'].put(num_images, wait=True)
+        else: # set camera to external triggering
+            # These are just in case the scan aborted with the camera in another state 
+            self.epics_pvs['CamTriggerMode'].put('On', wait=True)     # VN: For PG we need to switch to On to be able to switch to readout overlap mode                                                               
+            self.epics_pvs['CamTriggerSource'].put('Line0', wait=True)
+            self.epics_pvs['CamTriggerOverlap'].put('ReadOut', wait=True)
+            self.epics_pvs['CamExposureMode'].put('Timed', wait=True)
+            self.epics_pvs['CamImageMode'].put('Multiple')            
+            self.epics_pvs['CamArrayCallbacks'].put('Enable')
+            self.epics_pvs['CamFrameRateEnable'].put(0)
+
+            self.epics_pvs['CamNumImages'].put(self.num_angles, wait=True)
+            self.epics_pvs['CamTriggerMode'].put('On', wait=True)
+            self.wait_pv(self.epics_pvs['CamTriggerMode'], 1)
+
+    def begin_scan(self):
+        """Performs the operations needed at the very start of a scan.
+
+        This does the following:
+
+        - Set data directory.
+
+        - Set the TomoScan xml files
+
+        - Calls the base class method.
+        
+        - Opens the front-end shutter.
+
+        - Sets the PSO controller.
+
+        - Creates theta array using list from PSO. 
+
+        - Turns on data capture.
+        """
+        log.info('begin scan')
+
+        # Set data directory
+        file_path = self.epics_pvs['DetectorTopDir'].get(as_string=True) + self.epics_pvs['ExperimentYearMonth'].get(as_string=True) + os.path.sep + self.epics_pvs['UserLastName'].get(as_string=True) + os.path.sep
+        self.epics_pvs['FilePath'].put(file_path, wait=True)
+
+        # set TomoScan xml files
+        self.epics_pvs['CamNDAttributesFile'].put('TomoScanDetectorAttributes.xml')
+        self.epics_pvs['FPXMLFileName'].put('TomoScanLayout.xml')
+
+        # Call the base class method
+        super().begin_scan()
+        # Opens the front-end shutter
+        self.open_frontend_shutter()
+        
+    def end_scan(self):
+        """Performs the operations needed at the very end of a scan.
+
+        This does the following:
+
+        - Calls ``save_configuration()``.
+
+        - Put the camera back in "FreeRun" mode and acquiring so the user sees live images.
+
+        - Sets the speed of the rotation stage back to the maximum value.
+
+        - Calls ``move_sample_in()``.
+
+        - Calls the base class method.
+
+        - Closes shutter.  
+
+        - Add theta to the raw data file. 
+
+        - Copy raw data to data analysis computer.      
+        """
+
+        if self.return_rotation == 'Yes':
+            # Reset rotation position by mod 360 , the actual return 
+            # to start position is handled by super().end_scan()
+            current_angle = self.epics_pvs['Rotation'].get() %360
+            self.epics_pvs['RotationSet'].put('Set', wait=True)
+            self.epics_pvs['Rotation'].put(current_angle, wait=True)
+            self.epics_pvs['RotationSet'].put('Use', wait=True)
+        # Call the base class method
+        super().end_scan()
+        # Close shutter
+        self.close_shutter()
+
+        # Stop the file plugin
+        self.epics_pvs['FPCapture'].put('Done')
+        self.wait_pv(self.epics_pvs['FPCaptureRBV'], 0)
+        # Add theta in the hdf file
+        self.add_theta()
+
+        # Copy raw data to data analysis computer    
+        if self.epics_pvs['CopyToAnalysisDir'].get():
+            log.info('Automatic data trasfer to data analysis computer is enabled.')
+            full_file_name = self.epics_pvs['FPFullFileName'].get(as_string=True)
+            remote_analysis_dir = self.epics_pvs['RemoteAnalysisDir'].get(as_string=True)
+            dm.scp(full_file_name, remote_analysis_dir)
+        else:
+            log.warning('Automatic data trasfer to data analysis computer is disabled.')
+    
+    def set_exposure_time(self, exposure_time=None):
+
+        camera_model = self.epics_pvs['CamModel'].get(as_string=True)        
+        if(camera_model=='Q-12A180-Fm/CXP-6'):
+            if exposure_time is None:
+                exposure_time = self.epics_pvs['ExposureTime'].value            
+            self.epics_pvs['CamAcquisitionFrameRate'].put(1/exposure_time, wait=True, timeout=10.0) 
+            self.epics_pvs['CamAcquireTime'].put(exposure_time, wait=True, timeout = 10.0)
+        else:
+            super.set_exposure_time(exposure_time)
+
+    def add_theta(self):
+        """Add theta at the end of a scan.
+        """
+        log.info('add theta')
+
+        full_file_name = self.epics_pvs['FPFullFileName'].get(as_string=True)
+        if os.path.exists(full_file_name):
+            try:                
+                with h5py.File(full_file_name, "a") as f:
+                    if self.theta is not None:                        
+                        unique_ids = f['/defaults/NDArrayUniqueId']
+                        hdf_location = f['/defaults/HDF5FrameLocation']
+                        total_dark_fields = self.num_dark_fields * ((self.dark_field_mode in ('Start', 'Both')) + (self.dark_field_mode in ('End', 'Both')))
+                        total_flat_fields = self.num_flat_fields * ((self.flat_field_mode in ('Start', 'Both')) + (self.flat_field_mode in ('End', 'Both')))                        
+                        
+                        proj_ids = unique_ids[hdf_location[:] == b'/exchange/data']
+                        flat_ids = unique_ids[hdf_location[:] == b'/exchange/data_white']
+                        dark_ids = unique_ids[hdf_location[:] == b'/exchange/data_dark']
+
+                        # create theta dataset in hdf5 file
+                        if len(proj_ids) > 0:
+                            theta_ds = f.create_dataset('/exchange/theta', (len(proj_ids),))
+                            theta_ds[:] = self.theta[proj_ids - proj_ids[0]]
+
+                        # warnings that data is missing
+                        if len(proj_ids) != len(self.theta):
+                            log.warning(f'There are {len(self.theta) - len(proj_ids)} missing data frames')
+                            missed_ids = [ele for ele in range(len(self.theta)) if ele not in proj_ids-proj_ids[0]]
+                            missed_theta = self.theta[missed_ids]
+                            # log.warning(f'Missed ids: {list(missed_ids)}')
+                            log.warning(f'Missed theta: {list(missed_theta)}')
+                        if len(flat_ids) != total_flat_fields:
+                            log.warning(f'There are {total_flat_fields - len(flat_ids)} missing flat field frames')
+                        if (len(dark_ids) != total_dark_fields):
+                            log.warning(f'There are {total_dark_fields - len(dark_ids)} missing dark field frames')
+            except:
+                log.error('Add theta: Failed accessing: %s', full_file_name)
+                traceback.print_exc(file=sys.stdout)
+
+        else:
+            log.error('Failed adding theta. %s file does not exist', full_file_name)
+
+    def wait_pv(self, epics_pv, wait_val, timeout=-1):
+        """Wait on a pv to be a value until max_timeout (default forever)
+           delay for pv to change
+        """
+
+        time.sleep(.01)
+        start_time = time.time()
+        while True:
+            pv_val = epics_pv.get()
+            if isinstance(pv_val, float):
+                if abs(pv_val - wait_val) < EPSILON:
+                    return True
+            if pv_val != wait_val:
+                if timeout > -1:
+                    current_time = time.time()
+                    diff_time = current_time - start_time
+                    if diff_time >= timeout:
+                        log.error('  *** ERROR: DROPPED IMAGES ***')
+                        log.error('  *** wait_pv(%s, %d, %5.2f reached max timeout. Return False',
+                                      epics_pv.pvname, wait_val, timeout)
+                        return False
+                time.sleep(.01)
+            else:
+                return True
+
+    def wait_frontend_shutter_open(self, timeout=-1):
+        """Waits for the front end shutter to open, or for ``abort_scan()`` to be called.
+
+        While waiting this method periodically tries to open the shutter..
+
+        Parameters
+        ----------
+        timeout : float
+            The maximum number of seconds to wait before raising a ShutterTimeoutError exception.
+
+        Raises
+        ------
+        ScanAbortError
+            If ``abort_scan()`` is called
+        ShutterTimeoutError
+            If the open shutter has not completed within timeout value.
+        """
+
+        start_time = time.time()
+        pv = self.epics_pvs['OpenShutter']
+        value = self.epics_pvs['OpenShutterValue'].get(as_string = True)
+        log.info('open shutter: %s, value: %s', pv, value)
+        elapsed_time = 0
+        while True:
+            if self.epics_pvs['ShutterStatus'].get() == int(value):
+                log.warning("Shutter is open in %f s", elapsed_time)
+                return
+            if not self.scan_is_running:
+                exit()
+            value = self.epics_pvs['OpenShutterValue'].get()
+            time.sleep(1.0)
+            current_time = time.time()
+            elapsed_time = current_time - start_time
+            log.warning("Waiting on shutter to open: %f s", elapsed_time)
+            self.epics_pvs['OpenShutter'].put(value, wait=True)
+            if timeout > 0:
+                if elapsed_time >= timeout:
+                   exit()
+
     def move_crl_in(self):
         """Moves the crl in.
         """
