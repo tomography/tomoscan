ce588b2b0dfc4f4af754e5749ca6319ee6879c66
Francesco De Carlo <decarlof@gmail.com>
2021-10-20 14:26:23 -0500

    finalizing #130

--- tomoscan/tomoscan.py
+++ tomoscan/tomoscan.py
@@ -132,9 +132,7 @@ class TomoScan():
             if model.find('Grasshopper3 GS3-U3-23S6M') != -1:
                 self.control_pvs['CamVideoMode']    = PV(camera_prefix + 'GC_VideoMode_RBV')
             if model.find('Blackfly S BFS-PGE-161S7M') != -1:
-                # when this camera powers on, ExposureAuto is set to 'Continuous'  
-                # but we need 'Off' to properly work with tomoscan
-                PV(camera_prefix + 'GC_ExposureAuto').put('Off')           
+                self.control_pvs['GC_ExposureAuto']    = PV(camera_prefix + 'GC_ExposureAuto')       
 
         if (manufacturer.find('Adimec') != -1):
             self.control_pvs['CamExposureMode']            = PV(camera_prefix + 'ExposureMode')
