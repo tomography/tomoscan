8b13c53c3155c07f960a13b6d018e72426a19fbc
Mark Rivers <rivers@cars.uchicago.edu>
2025-02-02 18:18:02 -0600

    Add support for Hamamatsu and Mikrotron cameras

--- tomoscan/tomoscan_13bm_pso.py
+++ tomoscan/tomoscan_13bm_pso.py
@@ -11,6 +11,7 @@ import math
 import os
 from tomoscan.tomoscan_pso import TomoScanPSO
 from tomoscan import log
+from epics import caput
 
 class TomoScan13BM_PSO(TomoScanPSO):
     """Derived class used for tomography scanning with EPICS at APS beamline 13-BM-D
@@ -46,31 +47,61 @@ class TomoScan13BM_PSO(TomoScanPSO):
         log.info('set trigger mode: %s', trigger_mode)
         # Stop acquisition if we are acquiring
         self.epics_pvs['CamAcquire'].put('Done', wait=True)
-        if trigger_mode == 'FreeRun':
-            self.epics_pvs['CamImageMode'].put('Continuous', wait=True)
-            self.epics_pvs['CamTriggerMode'].put('Off', wait=True)
-            self.epics_pvs['CamAcquire'].put('Acquire')
-        elif trigger_mode == 'Internal':
-            self.epics_pvs['CamTriggerMode'].put('Off', wait=True)
-            self.epics_pvs['CamImageMode'].put('Multiple')
-            self.epics_pvs['CamNumImages'].put(num_images, wait=True)
-        else: # set camera to external triggering
-            self.epics_pvs['CamNumImages'].put(num_images, wait=True)
-            self.epics_pvs['CamTriggerMode'].put('On', wait=True)
-            self.epics_pvs['CamExposureMode'].put('Timed', wait=True)
-            self.epics_pvs['CamTriggerOverlap'].put('ReadOut', wait=True)
-            # There is a problem with the Grasshopper3 when switching to external trigger mode.
-            # The first 3 images are bad, at least at long exposure times.
-            # We take 3 dummy frames with Software trigger mode and don't save them to HDF5 file.
-            self.epics_pvs['CamImageMode'].put('Single', wait=True)
-            self.epics_pvs['CamTriggerSource'].put('Software', wait=True)
-            exposure = self.epics_pvs['CamAcquireTimeRBV'].value
-            self.epics_pvs['FPEnableCallbacks'].put('Disable', wait=True)
-            for i in range(3):
+        manufacturer = self.control_pvs['CamManufacturer'].get(as_string=True)
+        model = self.control_pvs['CamModel'].get(as_string=True)
+        if (manufacturer.find('Point Grey') != -1) or (manufacturer.find('FLIR') != -1):
+            if trigger_mode == 'FreeRun':
+                self.epics_pvs['CamImageMode'].put('Continuous', wait=True)
+                self.epics_pvs['CamTriggerMode'].put('Off', wait=True)
                 self.epics_pvs['CamAcquire'].put('Acquire')
-                time.sleep(.1)
-                self.epics_pvs['CamTriggerSoftware'].put(1, wait=True)
-                self.wait_camera_done(exposure + 5)
-            self.epics_pvs['FPEnableCallbacks'].put('Enable', wait=True)
-            self.epics_pvs['CamImageMode'].put('Multiple', wait=True)
-            self.epics_pvs['CamTriggerSource'].put('Line0', wait=True)
+            elif trigger_mode == 'Internal':
+                self.epics_pvs['CamTriggerMode'].put('Off', wait=True)
+                self.epics_pvs['CamImageMode'].put('Multiple')
+                self.epics_pvs['CamNumImages'].put(num_images, wait=True)
+            else: # set camera to external triggering
+                self.epics_pvs['CamNumImages'].put(num_images, wait=True)
+                self.epics_pvs['CamTriggerMode'].put('On', wait=True)
+                self.epics_pvs['CamExposureMode'].put('Timed', wait=True)
+                self.epics_pvs['CamTriggerOverlap'].put('ReadOut', wait=True)
+                # There is a problem with the Grasshopper3 when switching to external trigger mode.
+                # The first 3 images are bad, at least at long exposure times.
+                # We take 3 dummy frames with Software trigger mode and don't save them to HDF5 file.
+                self.epics_pvs['CamImageMode'].put('Single', wait=True)
+                self.epics_pvs['CamTriggerSource'].put('Software', wait=True)
+                exposure = self.epics_pvs['CamAcquireTimeRBV'].value
+                self.epics_pvs['FPEnableCallbacks'].put('Disable', wait=True)
+                for i in range(3):
+                    self.epics_pvs['CamAcquire'].put('Acquire')
+                    time.sleep(.1)
+                    self.epics_pvs['CamTriggerSoftware'].put(1, wait=True)
+                    self.wait_camera_done(exposure + 5)
+                self.epics_pvs['FPEnableCallbacks'].put('Enable', wait=True)
+                self.epics_pvs['CamImageMode'].put('Multiple', wait=True)
+                self.epics_pvs['CamTriggerSource'].put('Line0', wait=True)
+        if (manufacturer.find('HAMAMATSU') != -1):
+            if trigger_mode == 'FreeRun':
+                self.epics_pvs['CamImageMode'].put('Continuous', wait=True)
+                caput('13HAMA1:cam1:HamaTriggerSource', 'Internal', wait=True)
+                self.epics_pvs['CamAcquire'].put('Acquire')
+            elif trigger_mode == 'Internal':
+                caput('13HAMA1:cam1:HamaTriggerSource', 'Internal', wait=True)
+                self.epics_pvs['CamImageMode'].put('Multiple')
+                self.epics_pvs['CamNumImages'].put(num_images, wait=True)
+            else: # set camera to external triggering
+                self.epics_pvs['CamNumImages'].put(num_images, wait=True)
+                caput('13HAMA1:cam1:HamaTriggerSource', 'External', wait=True)
+                self.epics_pvs['FPEnableCallbacks'].put('Enable', wait=True)
+                self.epics_pvs['CamImageMode'].put('Multiple', wait=True)
+        if (manufacturer.find('Mikrotron GmbH') != -1):
+            if trigger_mode == 'FreeRun':
+                self.epics_pvs['CamImageMode'].put('Continuous', wait=True)
+                self.epics_pvs['CamTriggerMode'].put('Off', wait=True)
+                self.epics_pvs['CamAcquire'].put('Acquire')
+            elif trigger_mode == 'Internal':
+                self.epics_pvs['CamTriggerMode'].put('Off', wait=True)
+                self.epics_pvs['CamImageMode'].put('Multiple')
+                self.epics_pvs['CamNumImages'].put(num_images, wait=True)
+            else: # set camera to external triggering
+                self.epics_pvs['CamNumImages'].put(num_images, wait=True)
+                self.epics_pvs['CamTriggerSource'].put('Line0', wait=True)
+                self.epics_pvs['CamTriggerMode'].put('On', wait=True)
