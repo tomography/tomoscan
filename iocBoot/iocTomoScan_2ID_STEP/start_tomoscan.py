d8544bcb4a5e92f893936ba213cdfb0890df52ca
Francesco De Carlo <decarlof@gmail.com>
2025-02-20 15:10:45 -0600

    2-ID epics IOC and python server tested OK

--- iocBoot/iocTomoScan_2ID_STEP/start_tomoscan.py
+++ iocBoot/iocTomoScan_2ID_STEP/start_tomoscan.py
@@ -3,7 +3,7 @@
 #     python -i start_tomoscan_2bm.py
 # The -i is needed to keep Python running, otherwise it will create the object and exit
 from tomoscan.tomoscan_2id_step import TomoScan2IDSTEP
-ts = TomoScan2BMSTEP(["../../db/tomoScan_settings.req",
+ts = TomoScan2IDSTEP(["../../db/tomoScan_settings.req",
                   "../../db/tomoScan_step_settings.req",
                   "../../db/tomoScan_2ID_settings.req"], 
                  {"$(P)":"2id:", "$(R)":"TomoScanStep:"})
