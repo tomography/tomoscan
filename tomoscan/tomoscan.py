b2027d2156b5d73dd4f2d1867162dd3571b5b71e
Francesco De Carlo <decarlof@gmail.com>
2026-03-02 23:54:09 -0600

    Add the camera ArraySizeX_RBV PV to control_pvs so derived classes can read the detector horizontal size in pixels (e.g. for motion blur calculation)

--- tomoscan/tomoscan.py
+++ tomoscan/tomoscan.py
@@ -127,6 +127,7 @@ class TomoScan():
         self.control_pvs['CamNDAttributesMacros']  = PV(camera_prefix + 'NDAttributesMacros')
         self.control_pvs['CamArrayCounterRBV']     = PV(camera_prefix + 'ArrayCounter_RBV')
         self.control_pvs['CamUniqueIdMode']        = PV(camera_prefix + 'UniqueIdMode')
+        self.control_pvs['ArraySizeX_RBV']         = PV(camera_prefix + 'ArraySizeX_RBV')
 
         # If this is a Point Grey camera then assume we are running ADSpinnaker
         # and create some PVs specific to that driver
