3ccf9d9bc4e74a40c3fa80cd8f2671687c6a38eb
Francesco De Carlo <decarlof@gmail.com>
2026-03-17 22:50:28 -0500

    Pass DetectorTopDir to scp() in 2bm, 32id, fpga_2bm, stream_7bm
    Propagate the local_top_dir fix to all remaining beamline end_scan
    methods that call dm.scp(). Without this, scp() appends the instrument
    subdirectory to a RemoteAnalysisDir that already contains it, producing
    a doubled path in the remote destination

--- tomoscan/tomoscan_2bm.py
+++ tomoscan/tomoscan_2bm.py
@@ -567,7 +567,8 @@ class TomoScan2BM(TomoScanHelical):
                 self.epics_pvs['ScanStatus'].put('fdt file transfer complete')
             elif copy_to_analysis_dir == 2:
                 log.info('Using scp')
-                dm.scp(full_file_name, remote_analysis_dir)
+                dm.scp(full_file_name, remote_analysis_dir,
+                       Path(self.epics_pvs['DetectorTopDir'].get()))
                 self.epics_pvs['ScanStatus'].put('scp file transfer complete')
             else:
                 log.warning('Automatic data trasfer to data analysis computer is disabled.')
