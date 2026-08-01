7b960ec895399f2eb0ea6388771da09bd0438949
Mark Rivers <rivers@cars.uchicago.edu>
2026-08-01 12:13:20 -0500

    Fix import

--- iocBoot/iocTomoScan_6BMB/start_tomoscan.py
+++ iocBoot/iocTomoScan_6BMB/start_tomoscan.py
@@ -2,7 +2,7 @@
 # To run this script type the following:
 #     python -i start_tomoscan.py
 # The -i is needed to keep Python running, otherwise it will create the object and exit
-from tomoscan.tomoscan_13bm_pso import TomoScan13BM_PSO
+from tomoscan.tomoscan_6bmb import TomoScan6BMB
 ts = TomoScan6BMB(["../../db/tomoScan_settings.req",
                    "../../db/tomoScan_PSO_settings.req", 
                    "../../db/tomoScan_PCO_settings.req", 
