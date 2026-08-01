9d5a34979eb9d471838818ce76715a053a0cd21e
Mark Rivers <rivers@cars.uchicago.edu>
2026-08-01 12:10:03 -0500

    Fix class name

--- iocBoot/iocTomoScan_6BMB/start_tomoscan.py
+++ iocBoot/iocTomoScan_6BMB/start_tomoscan.py
@@ -3,8 +3,8 @@
 #     python -i start_tomoscan.py
 # The -i is needed to keep Python running, otherwise it will create the object and exit
 from tomoscan.tomoscan_13bm_pso import TomoScan13BM_PSO
-ts = TomoScan6BMB_PSO(["../../db/tomoScan_settings.req",
-                       "../../db/tomoScan_PSO_settings.req", 
-                       "../../db/tomoScan_PCO_settings.req", 
-                       "../../db/tomoScan_6BMB_settings.req"], 
-                      {"$(P)":"6BMB_LVPPS1:", "$(R)":"TS:"})
+ts = TomoScan6BMB(["../../db/tomoScan_settings.req",
+                   "../../db/tomoScan_PSO_settings.req", 
+                   "../../db/tomoScan_PCO_settings.req", 
+                   "../../db/tomoScan_6BMB_settings.req"], 
+                   {"$(P)":"6BMBTOMO:", "$(R)":"TS:"})
